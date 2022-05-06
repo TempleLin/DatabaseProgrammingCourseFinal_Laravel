c55f0ce315046efb621dbd466fbc4f26de3feea5
TempleLo <templelo5408@gmail.com>
2022-05-06 14:31:14 +0800

    First runnable wave.js library music visualization.

--- resources/views/sound_player.blade.php
+++ resources/views/sound_player.blade.php
@@ -1,22 +1,102 @@
 @extends('includes/template')
 
+{{--Docs for Wave.js: https://foobar404.dev/Wave.js/docs/--}}
+
 @section('content')
-    <link rel="stylesheet" href="{{asset('css/responsive_image_gallery.css')}}">
+    <link rel="stylesheet" href="{{asset('css/audio_player.css')}}">
     <div id="tsparticles"></div>
-    <div class="center">
+    <div class="audio_player_div center">
+        <canvas></canvas>
+        <br>
         <audio controls>
             <source src='{{$fileLink}}'>
         </audio>
     </div>
-{{--    <!-- Page content -->--}}
-{{--    <div class="main">--}}
-{{--        <div id="root"></div>--}}
-{{--    </div>--}}
 @endsection
 
 @section('page_end_declares')
-    {{--    <script src="{{asset('js/jsx/image_gallery.js')}}" type="text/babel"></script>--}}
-    <script src="{{asset('js/animations.js')}}"></script>
+    <script src="https://cdn.jsdelivr.net/gh/foobar404/wave.js/dist/bundle.js"></script>
+    <script>
+        let canvas = document.querySelector("canvas");
+        let audio = document.querySelector("audio");
+        let wave = new Wave(audio, canvas);
+
+        canvas.height = window.innerHeight - 30;
+        canvas.width = window.innerWidth - 30;
+
+        // // Simple example: add an animation
+        // wave.addAnimation(new wave.animations.Wave());
+
+        //
+        // Intermediate example: add an animation with options
+        // wave.addAnimation(new wave.animations.Arcs({
+        //     lineWidth: 10,
+        //     lineColor: "red",
+        //     fillColor: 'white',
+        //     count: 20
+        // }));
+        // wave.addAnimation(new wave.animations.Cubes({
+        //     count: 20,
+        //     cubeHeight: 100,
+        //     frequencyBand: 'highs',
+        //     gap: 3,
+        //     top: true,
+        //     right: true,
+        //     bottom: true,
+        //     left: true,
+        //     center: true
+        // }));
+
+        wave.addAnimation(new wave.animations.Cubes({
+            bottom: true,
+            count: 60,
+            cubeHeight: 20,
+            fillColor: { gradient: ["#FAD961", "#F76B1C"] },
+            lineColor: "rgba(0,0,0,0)",
+            radius: 10
+        }));
+        wave.addAnimation(new wave.animations.Cubes({
+            top: true,
+            count: 60,
+            cubeHeight: 20,
+            fillColor: { gradient: ["#FAD961", "#F76B1C"] },
+            lineColor: "rgba(0,0,0,0)",
+            radius: 10
+        }));
+        wave.addAnimation(new wave.animations.Circles({
+            lineColor: { gradient: ["#FAD961", "#FAD961", "#F76B1C"], rotate: 90 },
+            lineWidth: 4,
+            diameter: 20,
+            count: 10,
+            frequencyBand: "base"
+        }));
+
+        // wave.addAnimation(new wave.animations.Wave({
+        //     lineWidth: 10,
+        //     lineColor: "blue",
+        //     fillColor: 'cyan',
+        //     count: 20
+        // }));
+
+        // // Expert example: add multiple animations with options
+        // wave.addAnimation(new wave.animations.Square({
+        //     count: 50,
+        //     diamater: 300
+        // }));
+
+        // wave.addAnimation(new wave.animations.Glob({
+        //     fillColor: {gradient: ["red","blue","green"], rotate: 45},
+        //     lineWidth: 10,
+        //     lineColor: "#fff"
+        // }));
+
+        // The animations will start playing when the provided audio element is played
+
+        // 'wave.animations' is an object with all possible animations on it.
+
+        // Each animation is a class, so you have to new-up each animation when passed to 'addAnimation'
+
+    </script>
 @endsection
 
 {{--Add additional page-owned js-related declarations here.--}}
