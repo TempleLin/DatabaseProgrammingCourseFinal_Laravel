fc77c59c2225077c52ec12577d7b1a0a10f79e87
TempleLo <templelo5408@gmail.com>
2022-06-05 00:12:27 +0800

    Make navbar on the top fixed position.

--- public/js/jsx/top_nav_bar.js
+++ public/js/jsx/top_nav_bar.js
@@ -7,56 +7,58 @@ class TopNavBar extends Component {
     }
     render() {
         return (
-            <nav className={"navbar"}>
-                <div className="d-flex">
-                    <div action="/musics" className={'top_nav_left_padding top_nav_top_padding'}>
-                        <button className="btn btn-sm btn-outline-success text-light round-btn"
-                                onClick={() => {
-                                    this.props.handlerSideNavContent(SIDENAV_MUSIC_CATEGORIES);
-                                    this.props.handlerUseContent(MUSICS_GALLERY);
-                                }}>Musics</button>
+            <div className={'top_navbar'}>
+                <nav className={"navbar"}>
+                    <div className="d-flex">
+                        <div action="/musics" className={'top_nav_left_padding top_nav_top_padding'}>
+                            <button className="btn btn-sm btn-outline-success text-light round-btn"
+                                    onClick={() => {
+                                        this.props.handlerSideNavContent(SIDENAV_MUSIC_CATEGORIES);
+                                        this.props.handlerUseContent(MUSICS_GALLERY);
+                                    }}>Musics</button>
+                        </div>
+                        <div action="/sounds" className={'top_nav_left_padding top_nav_top_padding'}>
+                            <button className="btn btn-sm btn-outline-success text-light round-btn"
+                                    onClick={() => {
+                                        this.props.handlerSideNavContent(SIDENAV_SOUND_CATEGORIES);
+                                        this.props.handlerUseContent(SOUNDS_GALLERY);
+                                    }}>Sounds</button>
+                        </div>
                     </div>
-                    <div action="/sounds" className={'top_nav_left_padding top_nav_top_padding'}>
-                        <button className="btn btn-sm btn-outline-success text-light round-btn"
-                                onClick={() => {
-                                    this.props.handlerSideNavContent(SIDENAV_SOUND_CATEGORIES);
-                                    this.props.handlerUseContent(SOUNDS_GALLERY);
-                                }}>Sounds</button>
-                    </div>
-                </div>
-                <div className="d-flex">
-                    <button className="btn btn-outline-success text-light round-btn" onClick={() => {this.props.handlerUseContent(UPLOAD_FORM)}}>Upload</button>
-                    <div action="/login_register">
-                        <button className="btn btn-outline-success text-light form-inline round-btn"
-                                onClick={() => {
-                                    if ($('#loggedIn').text() !== '1') {
-                                        this.props.handlerUseContent(LOGIN_FORM)
-                                    } else {
-                                        $.ajax({
-                                            headers: {
-                                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') //CSRF Token related to Laravel.
-                                            },
-                                            url:'/logout',
-                                            method: "GET",
-                                            dataType: 'text', //Response to expect.
-                                            data: null,
-                                            // processData: false, //Stops jQuery processing any of the data.
-                                            // contentType: false, //Forces jQuery not to add a Content-Type header.
-                                            success:function (data) {
-                                                if (data === 'LOGOUT_SUCCESS') {
-                                                    location.reload();
+                    <div className="d-flex">
+                        <button className="btn btn-outline-success text-light round-btn" onClick={() => {this.props.handlerUseContent(UPLOAD_FORM)}}>Upload</button>
+                        <div action="/login_register">
+                            <button className="btn btn-outline-success text-light form-inline round-btn"
+                                    onClick={() => {
+                                        if ($('#loggedIn').text() !== '1') {
+                                            this.props.handlerUseContent(LOGIN_FORM)
+                                        } else {
+                                            $.ajax({
+                                                headers: {
+                                                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') //CSRF Token related to Laravel.
+                                                },
+                                                url:'/logout',
+                                                method: "GET",
+                                                dataType: 'text', //Response to expect.
+                                                data: null,
+                                                // processData: false, //Stops jQuery processing any of the data.
+                                                // contentType: false, //Forces jQuery not to add a Content-Type header.
+                                                success:function (data) {
+                                                    if (data === 'LOGOUT_SUCCESS') {
+                                                        location.reload();
+                                                    }
+                                                },error:function(data){
+                                                    console.log(data);
                                                 }
-                                            },error:function(data){
-                                                console.log(data);
-                                            }
-                                        });
-                                    }
-                                }}>
-                            {($('#loggedIn').text() !== '1')? <span>Login/Register</span> : <span>Logout</span>}
-                        </button>
+                                            });
+                                        }
+                                    }}>
+                                {($('#loggedIn').text() !== '1')? <span>Login/Register</span> : <span>Logout</span>}
+                            </button>
+                        </div>
                     </div>
-                </div>
-            </nav>
+                </nav>
+            </div>
         );
     }
 }
